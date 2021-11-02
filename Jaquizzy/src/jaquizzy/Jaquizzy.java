be4bbbb19d48f15c7f432cc0c893971f5da9ef7f
Advaith Karnad <advaith.k.246@gmail.com>
2021-11-02 15:34:07 +0530

    Added swing-GUI base

--- Jaquizzy/src/jaquizzy/Jaquizzy.java
+++ Jaquizzy/src/jaquizzy/Jaquizzy.java
@@ -1,21 +1,30 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package jaquizzy;
+
+import java.awt.Color;
+
+import javax.swing.*;
+import java.awt.*;
 
 /**
  *
- * @author akarn
+ * @author Advaith Karnad : AxyleneOmega
+ * @author Fiza Sameer : bbq-sauce
  */
-public class Jaquizzy {
+public class Jaquizzy extends JFrame{
+    Jaquizzy(){
+        setBounds(200, 100, 800, 600);
+        getContentPane().setBackground(Color.BLACK);
+        setLayout(null);
 
-    /**
-     * @param args the command line arguments
-     */
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("jaquizzy/Assets/Placeholder.png"));
+        JLabel l1 = new JLabel(i1);
+        l1.setBounds(0, 0, 400, 300);
+        add(l1);
+
+        setVisible(true);
+    }
     public static void main(String[] args) {
-        // TODO code application logic here
+        new Jaquizzy();
     }
     
 }
